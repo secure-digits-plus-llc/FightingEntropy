8ef5c22956ff3255e1c8d38b4d5242d58f84eb84
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2020-01-09 00:27:32 -0500

    Update index.js
    Bayes Theorem here we come.
    
    Maybe I'm not that dumb after all.

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-    
-    var windowWidth = window.innerWidth;
+
+    var start                  = window.innerWidth;
+    var scale                  = start;
+    var wait                   = true;
+    var font                   = 48;
+    var h1                     = document.querySelector('h1');
+
+    h1.style.setProperty("--h1-size",font);
 
     function getWindowWidth()
     {
-        if ( windowWidth != window.innerWidth )
+        if ( scale    != window.innerWidth )
         {
-            var windowWidth = window.innerWidth;
+            scale              = window.innerWidth
+            font               = scale / 28
+
+            var h1             = document.querySelector('h1');
+            h1.style.setProperty("--h1-size",font);
+
+            if ( wait == true )
+            {
+                console.log( scale );
+
+                wait = false;
+
+                setTimeout( () =>
+                {
+                    wait = true;
+
+                }, 200 );
+            }
         }
-
-        setTimeout(() => 
-        {
-            console.log( windowWidth );
-
-        }, 500 );
     }
 
     getWindowWidth();
-     
-    window.addEventListener( "resize" , getWindowWidth );
 
-
-    function mobileStyleSheet()
-    {
-        console.log("so maybe this guy can program javascript too...");
-    }
+    window.addEventListener("resize",getWindowWidth);
