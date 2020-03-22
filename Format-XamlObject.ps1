411f0e48371db6126ca98731219511a3c7685f60
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2020-03-22 17:42:24 -0400

    Update Format-XamlObject.ps1
    WELP, *its harder than I thought... to try and manage the interweavings of codified objects that sometimes skip a line or enter another hash table while nobody's looking...
    
    Of course, that would drive only the most meticulous bastards out there insane...

--- Format-XamlObject.ps1
+++ Format-XamlObject.ps1
@@ -1,14 +1,10 @@
 Function Format-XamlObject # Formats a Xaml Object to have a clean structure ________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
 {#/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯      
-    [ CmdLetBinding () ] Param ( # Still revising
+    [ CmdLetBinding () ] Param ( 
         
         [ Parameter ( Mandatory , Position = 0 ) ] [ String ] $Xaml )
 
-        # [ Custom Types / Script Blocks ]
-
-        $Script                                 = [ PSCustomObject ]@{ 
-        
-            Reg                                 = "(?<=\<).+?(?=\>)"   
+        $Script                                 = [ PSCustomObject ]@{
 
             Object                              = {
 
@@ -16,8 +12,7 @@ Function Format-XamlObject # Formats a Xaml Object to have a clean structure ___
 
                 [ PSCustomObject ]@{ 
 
-                    Track                       = ForEach ( $I in 0..( $Xaml.Split( "`n" ).Count - 1 ) ) 
-
+                    Track                       = ForEach ( $I in 0..( $Xaml.Split( "`n" ).Count - 1 ) )
                     {
                         [ PSCustomObject ]@{ 
 
@@ -27,27 +22,30 @@ Function Format-XamlObject # Formats a Xaml Object to have a clean structure ___
                     }
                     Xaml                        = $Xaml 
                     Array                       = $Xaml.Split("`n") 
-                    Items                       = $Xaml.Split("`n").Split(' ') | ? { $_.Length -gt 0 } 
+                    Items                       = $Xaml.Split("`n").Split(' ') | ? { $_.Length -gt 0 }
                 }
             }
         
             Template                            = { 
 
-            Param ( $Index , $Drive )
+                Param ( $Index , $Drive )
 
                 [ PSCustomObject ]@{
                         
+                    Index                       = $Index
                     Drive                       = $Drive
-                    Parent                      = ""
+                    Track                       = ""
+                    Type                        = ""
                     Path                        = ""
+                    Parent                      = ""
                     Name                        = ""
+                    Slice                       = ""
                     Depth                       = ""
                     Indent                      = ""
-                    Track                       = ""
-                    Index                       = $Index
-                    Slice                       = ""
-                    Property                    = @{ }
-                    Children                    = @( )
+                    End                         = ""
+                    Tag                         = ""
+                    Property                    = $Null
+                    Value                       = $Null
                 }
             }
         }
@@ -58,31 +56,36 @@ Function Format-XamlObject # Formats a Xaml Object to have a clean structure ___
     #        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯  #
         Begin
         {
-            $Root                               = & $Script.Object $Xaml_Input
             $Drive                              = "DSC:"
             $Name                               = $Null
             $Path                               = $Null
+            $Root                               = & $Script.Object $Xaml_Input
             $Depth                              = 0
+            $Type                               = "@"
+            $Indent                             = 0
             $Return                             = $Root.Items -match "<"
             $Count                              = $Return.Count - 1
-    #     _______________________________________________________________________________________________________________  #
-    #    /¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯[ Start All Templates ]¯\ #
-    #    \_______________________________________________________________________________________________________________/ #
-
             $Index                              = 0
             $X                                  = 0
             $I                                  = 0
             $T                                  = @( )
-
+        }
+    #    ____    ____________________________________________________________________________________________________      #
+    #   //¯¯\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\___  #
+    #   \\__//¯¯¯           Process [~] : Re-track and Re-calculate positioning of Objects                       ___//¯¯\\ #
+    #    ¯¯¯\\__________________________________________________________________________________________________//¯¯\\__// #
+    #        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯  #
+        Process
+        {
             ForEach ( $X in 0..$Count )
             {
-                $T                             += & $Script.Template $X $Drive 
+                Write-Progress -Activity "Collecting [~] Track Information" -PercentComplete ( ( $X / $Count ) * 100 )
 
-                $T[$X].Name                     = $Return[$X]
-                #-------------
+                $T                             += & $Script.Template $X $Drive
 
+                $T[$X].Tag                      = $Return[$X] -Replace "(\s+)" , ""
+                $T[$X].Type                     = $Type                
                 $T[$X].Track                    = $Return[$X]
-
                 If ( $T[$X].Track -notmatch ">" )
                 {
                     While( $T[$X].Track -notmatch ">" )
@@ -92,47 +95,189 @@ Function Format-XamlObject # Formats a Xaml Object to have a clean structure ___
                     }
                 }
                 $Index ++
+                $T[$X].Track                    = $T[$X].Track -Replace "(\s+)" , " " -Replace "(\'\s\/>)" , "'/>" -Replace "(\'\s[>])" , "'>"
+                $T[$X].Slice                    = $T[$X].Track -Replace ( $T[$X].Tag + " " ) , "" -Replace "' " , "';" -Split ";"
 
-                $T[$X].Track                    = $T[$X].Track -Replace "(\'\s\/>)" , "'/>" -Replace "(\'\s[>])" , "'>"
-                #-------------
+                If ( $T[$X].Slice.Count -gt 1 )
+                {
+                    If ( $T[$X].Slice.Count -eq 1 )
+                    {
+                        $T[$X].Property         = $T[$X].Slice.Split(' ')[0]
+                        $T[$X].Value            = $T[$X].Slice -Replace ( $T[$X].Property + " = " ) , ''
+                    }
 
-                $T[$X].Depth                    = $T[$X].Track | % {
-
-                        If ( $_ -Match "(<[^/].+[^/]>)" ) { "+" } # Indent
-                    ElseIf ( $_ -Match ".+(\/>)"        ) { "/" } # No indent
-                    ElseIf ( $_ -Match "(<\/).+"        ) { "-" } # Subtract Indent
-                    Else                                  { "?" } # Item is unknown
+                    Else
+                    {
+                        $T[$X].Property         = 0..( $T[$X].Slice.Count - 1 ) | % { $T[$X].Slice[$_].Split(' ')[0] }
+                        $T[$X].Value            = 0..( $T[$X].Slice.Count - 1 ) | % { $T[$X].Slice[$_] -Replace ( $T[$X].Property[$_] + " = " ) , '' }
+                    }
                 }
-                # -----------
 
-                $T[$X].Slice                    = $T[$X].Track -Replace ( $Return[$X] + " " ) , "" -Replace "' " , "';" -Split ";"
-                
-                # -----------
+                $Type                           = $T[$X].Track | % {
 
-                $T[$X].Name                     = $Return[$X]  -Replace "(<*/*\>*)" , ""
+                    If     ( $_ -Match "(<[^/].+[^/]>)" ) { "+" }
+                    ElseIf ( $_ -Match ".+(\/>)"        ) { "/" }
+                    ElseIf ( $_ -Match "(<\/).+"        ) { "-" }
+                    Else                                  { "?" }
+                }
+
+                $T[$X].Name                     = $Return[$X] -Replace "(<*/*\>*)" , ""
 
                 If ( $Path -ne $Null )
                 {
-                    $T[$X].Path                 = @{
-
-                            "+"                 = $Path , $T[$X].Name -join '\'
-                            "/"                 = $Path
-                            "-"                 = Split-Path $Path -Parent
-    
-                    }[ $T[$X].Depth ]
+                    $T[$X].Path                 = @{ 
+                        
+                        "+"                     = $Path , $T[$X].Name -join '\'
+                        "/"                     = $Path 
+                        "-"                     = Split-Path $Path -Parent 
+                    
+                    }[ $T[$X].Type ]
 
                     $Path                       = $T[$X].Path
+
+                    $T[$X].Parent               = Split-Path $Path -Parent | % {
+                        
+                        If ( $_.Length -eq "" -or $_ -eq $Drive )
+                        {
+                            "-"
+                        }
+
+                        Else 
+                        {
+                            $_
+                        }
+                    }
                 }
 
                 If ( $Path -eq $Null )
                 {
                     $Name                       = $T[$X].Name
                     $Path                       = $Drive , $Name -join '\'
+                    $T[$X].Parent               = $Drive
                     $T[$X].Path                 = $Path
                 }
 
-                $T[$X].Parent                   = Split-Path $Path -Parent
-                $T[$X].Indent                   = $Path.Split('\').Count - 1
+                $Depth                          = $Path.Split('\').Count - 2
+                $T[$X].Depth                    = $Depth
 
+                If ( $Sw0 -ne $True  -and $T[$X].Tag   -match "(<\/)"  -and $T[$X].Type -eq "/" )
+                { 
+                    $Sw0 = $True
+                } 
 
+                If ( $Sw0 -ne $False -and $T[$X].Tag -notmatch "(<\/)" -and $T[$X].Type -eq "+" )
+                {
+                    $Sw0 = $False
+                }
+
+                $T[$X].Indent                   = @{
+                    
+                    "@"                         = 0
+                    "/"                         = If ( $Sw0 ) { $Depth - 3 } Else { $Depth - 1 }
+                    "-"                         = If ( $Sw0 ) { $Depth - 2 } Else { $Depth - 1 }
+                    "+"                         = $Depth
+
+                }[ $Type ]
             }
+        }
+    #    ____    ____________________________________________________________________________________________________      #
+    #   //¯¯\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\___  #
+    #   \\__//¯¯¯     End Parameter [~] : Finalize the object arrangement for output                             ___//¯¯\\ #
+    #    ¯¯¯\\__________________________________________________________________________________________________//¯¯\\__// #
+    #        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯  #
+        End
+        {
+            $Buffer                             = [ PSCustomObject ]@{  # Numbers only
+
+                Indent                          = ""
+                MaxIndent                       = ( $T.Indent                     | Sort )[ -1 ] * 4
+                PostIndent                      = ""
+                OutputIndent                    = ""
+
+                Tag                             = ""
+                MaxTag                          = ( $T.Tag      | % { $_.Length } | Sort )[ -1 ]
+                PostTag                         = ""
+                OutputTag                       = ""
+
+                Property                        = ""
+                MaxProperty                     = ( $T.Property | % { $_.Length } | Sort )[ -1 ]
+                PostProperty                    = ""
+                OutputProperty                  = ""
+            }
+            
+            ForEach ( $X in 0..$Count )
+            {
+                $Buffer                         | % { 
+
+                    $_.Indent                   = $T[$X].Indent * 4
+                    $_.PostIndent               = $_.MaxIndent - $_.Indent
+                    $_.OutputIndent             = ( " " * $_.Indent ) 
+                
+                    $_.Tag                      = $T[$X].Tag.Length
+                    $_.PostTag                  = $_.MaxTag - $_.Tag
+                    $_.OutputTag                = $T[$X].Tag
+
+                    "{0}{1}" -f $_.OutputIndent , $_.OutputTag
+                }
+            }
+
+                
+                $Indent                         = $T[$X].Indent * 4
+                $PostIndent                     = $Indent
+                $Item                           = $T[$X].Tag.Length 
+                $Space                          = $Buffer.Left + $Buffer.Tag 
+                
+                - ( $Indent + $Item )
+
+                    $Max                        = [ PSCustomObject ]@{ 
+
+                        Margin                  = $Indent 
+                        Item                    = $T[$X].Tag
+                        Space                   = $Buffer.Left - ( $T[$X].Indent * 4 )  
+                        Indent                  = $Buffer.Left
+                        Item                    = $Buffer.Tag
+                    }
+                }
+
+                "{0} {1}" -f $Indent , $Item
+                
+                # ____________________________________________________                                                     | MaxIndent  
+                #                                                     _______________________________________
+                # [        ]                             # Actual Indent                                                   | MaxIndent   
+                #           [       ]                    # Actual Tag                                                      | MaxTag
+                # [                                    ] # [Indent] + [Item buffer] = Maximum length                       | MaxLength
+                #                    [                 ] # Remaining Space                               | MaxSpace
+
+
+                # /__ General __/
+                # Indent                    = $Buffer | % { $_.Left + $_.Tag }
+                # Tag                       = 
+
+                $MaxIndent                      = $Buffer | % { $_.Left * 4 }    # {0}[ Maximum Indent/Margin Buffer ]
+                $MaxSpace                       = $MaxIndent + $Buffer.Tag       # {1}[ Maximum     Space/Tag Buffer ]
+                $MaxProperty                    = $Buffer.Property + $MaxSpace   # {2}[ Maximum      Property Buffer ]
+
+                If ( $T[$X].Property.Count -eq 0 )
+                {   
+
+                }
+
+                If ( $T[$X].Property.Count -eq 1 )
+                {
+
+                }
+
+                If ( $T[$X].Property.Count -gt 1 )
+                {
+
+                    ForEach ( $J in 0..( $T[$X].Property.Count - 1 ) )
+                    {
+                        If ( $J -eq 0 )
+                        {
+                        
+                        }
+
+                    }
+                }
+            }
+        }
