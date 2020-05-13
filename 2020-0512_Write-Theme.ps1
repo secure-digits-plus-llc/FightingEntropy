785b20df42091b8c25dbdac4d2b5e7c32f399281
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2020-05-12 21:14:28 -0400

    Update 2020-0512_Write-Theme.ps1

--- 2020-0512_Write-Theme.ps1
+++ 2020-0512_Write-Theme.ps1
@@ -1,36 +1,19 @@
 
-
-    [Environment]::GetEnvironmentVariables().GetEnumerator() | Sort Name 
-    [System.Management.Automation.Host.Size]::new(128,5000)
-
-Function Write-Theme
-{ 
-    [ CmdLetBinding ( <# DefaultParameterSetName = "" #> ) ] Param (
-
-        #[ Parameter ( Mandatory, Position=0, ValueFromPipeline=$True, ParameterSetName=     "XML" )][String]       $XML ,
-        [ Parameter ( Mandatory, Position=0, ValueFromPipeline=$True, ParameterSetName= "Function" )][String]  $Function ,
-        [ Parameter ( Mandatory, Position=0, ValueFromPipeline=$True, ParameterSetName=   "Action" )][String]    $Action ,
-        [ Parameter ( Mandatory, Position=0, ValueFromPipeline=$True, ParameterSetName=  "Section" )][String]   $Section ,
-        [ Parameter ( Mandatory, Position=0, ValueFromPipeline=$True, ParameterSetName=    "Table" )][String]     $Table )
-        #[ Parameter ( Mandatory, Position=1, ValueFromPipeline=$True, ParameterSetName=   "Action" )]
-        #[ Parameter ( Mandatory, Position=1, ValueFromPipeline=$True, ParameterSetName= "Function" )][String]      $Slot ,
-        #[ Parameter (            Position=2, ValueFromPipeline=$True, ParameterSetName=   "Action" )]
-        #[ Parameter (            Position=2, ValueFromPipeline=$True, ParameterSetName= "Function" )][String]      $Info ,
-        #[ Parameter ( Mandatory, Position=1, ValueFromPipeline=$True, ParameterSetName=  "Section" )]
-        #[ Parameter ( Mandatory, Position=1, ValueFromPipeline=$True, ParameterSetName=    "Table" )][String]     $Title ,
-        #[ Parameter ( Mandatory, Position=2, ValueFromPipeline=$True, ParameterSetName=  "Section" )]
-        #[ Parameter ( Mandatory, Position=2, ValueFromPipeline=$True, ParameterSetName=    "Table" )][String]    $Prompt ,
-        #[ Parameter ( Mandatory, Position=3, ValueFromPipeline=$True, ParameterSetName=    "Color" )][HashTable] $Colors )
-
-    Class Range 
+    Class Range
     {
-        [Int32]   $Total
         [Int32[]] $Range
 
-        Range([Int32]$Total)
-        {
-            $This.Total = $Total
-            $This.Range = $( If ( $Total -gt 1 ) { 0..( $Total - 1 ) } Else { Throw "Not a range" } )
+        Range([Int32]$Total) 
+        { 
+            If ( $Total -gt 1 ) 
+            { 
+                $This.Range = [Int32[]]@( 0..( $Total - 1 ) )
+            }
+            
+            Else 
+            { 
+                Throw "Not a range" 
+            }
         }
     }
 
@@ -44,14 +27,13 @@ Function Write-Theme
         [String]  $Hexidecimal
         [String]  $Hex
 
-        Charm([Int]$Index)
+        Charm([Int32]$Index)
         {
-            $This.Index        = [Int]   $Index
+            $This.Index        = [Int32] $Index
             $This.Char         = [Char]  $Index
-
             $This.Name         = @{
             
-                $True          = @{
+                $True          = @{ 
                 
                     32         = "nbsp"
                     34         = "quot"
@@ -74,153 +56,65 @@ Function Write-Theme
         }
     }
 
+    Class Block
+    {
+        static [String[]] $Full = "    ","____","¯¯¯¯","----","   /","\   ","   \","/   ","\__/","/¯¯\","/¯¯¯","¯¯¯\","\___","___/"
+    }
+
+    Class Stock
+    {
+        [String[]] $Margin
+        [String[]] $Face
+        [String[]] $Fill
+
+        Stock([Int32]$Index)
+        {
+            $This.Margin = [Block]::Full[@{ 0 =  4,  5 ; 1 =  6,  7 }[ $Index % 2 ] ]
+            $This.Face   = [Block]::Full[@{ 0 =  8,  9 ; 1 =  9,  8 }[ $Index % 2 ] ]
+            $This.Fill   = [Block]::Full[@{ 0 = 10, 11 ; 1 = 12, 13 }[ $Index % 2 ] ]
+        }
+    }
+
     Class Track
     {
-                  [Int32] $Index
-                 [String] $Track
-        static [String[]] $Block   = "    ","____","¯¯¯¯","----"
-               [String]   $Mask 
-               [String[]] $Base
-               [String[]] $Margin
-               [String[]] $Fill
+        [Int32]    $Index
+        [String]   $Track
+        [String[]] $Face
+        [String[]] $Items
+        [Int32[]]  $Guide
+        [String[]] $String
+        [String[]] $Fill
+        [String]   $Serial
+        [String]   $Mask
 
         Track([Int32]$Index,[String]$Track)
         {
-            $This.Index     = [Int32]  $Index
-            $This.Track     = [String] $Track
-            $This.Base      = @{ 0 = "\__/","/¯¯\" ; 1 = "/¯¯\","\__/" }[ $Index % 2 ]
-            $This.Margin    = @{ 0 = "   /","\   " ; 1 = "   \","/   " }[ $Index % 2 ]
-            $This.Fill      = @{ 0 = "/¯¯¯","¯¯¯\" ; 1 = "\___","___/" }[ $Index % 2 ]
-        }
-    }
+            If ( $Track.Length -lt 120 ) { Throw "Does not meet minimum length" }
 
-    Class Section
-    { 
-        [String]  $Title
-        [Int32[]] $Index
-        [Track[]] $Stack
+            $This.Index    = $Index
+            $This.Track    = $Track
+            $This.Face     = 0..( $Track.Length - 1 ) | ? { $_ % 4 -eq 0 } | % { $Track.Substring( $_ , 4 ) }
+            $This.Items    = $This.Face | ? { $_ -in [Block]::Full } | Select -Unique
+            $This.Guide    = 0..( $This.Face.Count - 1 ) | ? { $This.Face[$_] -in $This.Items } | % { $I = $_ ; 0..( $This.Items.Count - 1 ) | ? { $This.Items[$_] -eq $This.Face[$I] } }
+            $This.String   = $This.Serial = $This.Guide -join ';'
+            $This.Fill     = ( [Regex]"([\d])+([;]\1){2,}" ).Matches( $This.String ).Value
 
-        Stack([Int32[]]$Index,[Track[]]$Stack)
-        {
-            $This.Index        = ForEach ( $I in $Index ) { $Index[$I] }
-            $This.Stack        = ForEach ( $I in $Index ) { $S[$I]     }
-        }
-    }
-
-    $Type        = @{
-                
-        Function = "Function"
-        Action   = "Action"
-        Section  = "Section"
-        Table    = "Table"
-            
-    }[ $PsCmdlet.ParameterSetName ]
-    
-    $Obj = [ PSCustomObject ]@{ 
-
-        
-        Charm = ([Range]255).Range | % { [Charm]$_ }
-        Track = ([Range]  5).Range | % { [Track]::New($_,$Array[$_]) }
-
-    }
-    # ------
-
-        $Test        = [Ordered]@{
-               
-            Example1 = [Ordered]@{ Item1_1 = "Value1_1"; Item1_2 = "Value1_2"; Item1_3 = "Value1_3" }
-            Example2 = [Ordered]@{ Item2_1 = "Value2_1"; Item2_2 = "Value2_2"; Item2_3 = "Value2_3" }
-            Example3 = [Ordered]@{ Item3_1 = "Value3_1"; Item3_2 = "Value3_2"; Item3_3 = "Value3_3" }
-        }
-            
-        $Type        = @{
-                
-            Function = "Function"
-            Action   = "Action"
-            Section  = "Section"
-            Table    = "Table"
-            
-        }[ $PsCmdlet.ParameterSetName ]
-
-        If ( $Type -match "(Action|Function)" )
-        {
-            
-        }
-
-        Else
-        {
-            
-        }
-    
-    }
-
-
-        If ( $Table )
-        {
-            $Keys = $Table | Select Keys
-
-            ForEach ( $Hashtable in $Hashtables )
+            If ( $This.Fill.Count -gt 1 )
             {
-                
+                ForEach ( $I in 0..( $This.Fill.Count - 1 ) )
+                {
+                    $This.String = $This.Fill[$I] | % { $This.String.Replace( $_ , ( "@({0})*{1}" -f ( $_.Split(";")[0] , $_.Split(";").Count ) ) ) }
+                }
             }
-        }
 
-        # ----------
+            Else
+            {
+                $This.String = $This.Fill | % { 
+                
+                    $This.String.Replace( $_ , ( "@({0})*{1}" -f ( $_.Split(";")[0] , $_.Split(";").Count ) ) ) }
+            }
 
+            $This.Mask     = ( $This.Serial.Split(';') | % { "{$_}" } ) -join ""
 
-
-    # 0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28   29   30
-    # 0    4    8   12   16   20   24   28   32   36   40   44   48   52   56   60   64   68   72   76   80   84   88   92   96  100   104 108  112  116  120
-    # |    |____|    |____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|    |    "
-    # |   /|/¯¯\|\__/|/¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯\|\___|    "
-    # |   \|\__/|/¯¯¯|   _|__[ |Tabl|e of| Con|tent|s ] |____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|_   |___/|/¯¯\|\   "
-    # |    |¯¯¯\|\___|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|___/|/¯¯\|\__/|/   "
-    # |    |    |¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|¯¯¯¯|    |¯¯¯¯|    "
-    # |    |____|    |____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|    |    "
-
-    $Sample  = "    ____    ____________________________________________________________________________________________________        ",
-               "   //¯¯\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\___    ",
-               "   \\__//¯¯¯   ___[ Content... For all tablekind. ( A normal, yet dramatic, table of content...) ]_______   ___//¯¯\\   ",
-               "    ¯¯¯\\__________________________________________________________________________________________________//¯¯\\__//   ",
-               "        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯    "
-    
-
-    $Array   = "    ____    ____________________________________________________________________________________________________        ",
-               "   //¯¯\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\___    ",
-               "   \\__//¯¯¯   ___[ Table of Contents ] _________________________________________________________________   ___//¯¯\\   ",
-               "    ¯¯¯\\__________________________________________________________________________________________________//¯¯\\__//   ",
-               "        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯    "
-
-               "   \\__//                                                                                                  \\__//¯¯\\   " 
-               "   //¯¯\\                                                                                                  //¯¯\\__//   " 
-
-               "   \",
-               "\__/",
-               "/  "
-
-
-    $Test   = @{ 
-               
-        Example1 = @{
-        
-            Item1_1 = "Value1_1"
-            Item1_2 = "Value1_2"
-            Item1_3 = "Value1_3"
-        }
-
-        Example2 = @{
-        
-            Item2_1 = "Value2_1"
-            Item2_2 = "Value2_2"
-            Item2_3 = "Value2_3"
-        }
-
-        Example3 = @{
-        
-            Item3_1 = "Value3_1"
-            Item3_2 = "Value3_2"
-            Item3_3 = "Value3_3"
         }
     }
-
-
-    
